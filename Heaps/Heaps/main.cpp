ed3d370b3c81631fcfcdb2956f5524b9635435da
RohanSoni-02 <soni.rohanchd@gmail.com>
2024-05-03 18:49:07 +0530

    added helper functions

--- Heaps/Heaps/main.cpp
+++ Heaps/Heaps/main.cpp
@@ -6,6 +6,32 @@
 //
 
 #include <iostream>
+#include <vector>
+using namespace std;
+
+class Heap{
+private:
+    vector<int> heap;
+    
+    int leftChild(int index){
+        return 2*index + 1;
+    }
+    
+    int rightChild(int index){
+        return 2*index + 2;
+    }
+    
+    int parent(int index){
+        return (index - 1)/2;
+    }
+    
+    void swap(int index1, int index2){
+        int temp = heap[index1];
+        heap[index1] = heap[index2];
+        heap[index2] = temp;
+    }
+    
+};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
